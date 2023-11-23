086235ad65d2bb9915773c59f5748450ea2452f1
JungMo <jmjm0611@naver.com>
2023-11-23 17:09:52 +0900

    기본적이 패키지 구조 변경4

---
+++ src/main/java/com/example/pj2be/controller/pagingcontroller/PagingController.java
@@ -0,0 +1,12 @@
+package com.example.pj2be.controller.pagingcontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/paging")
+public class PagingController {
+
+}
---
+++ src/main/java/com/example/pj2be/mapper/pagingmapper/PagingMapper.java
@@ -0,0 +1,7 @@
+package com.example.pj2be.mapper.pagingmapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class PagingMapper {
+}
