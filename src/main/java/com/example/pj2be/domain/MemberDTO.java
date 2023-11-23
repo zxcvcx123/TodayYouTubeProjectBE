ab47fabad075efe82cc6d3bd35d9a8342b3a71dc
jay sung <namic123@naver.com>
2023-11-23 20:50:56 +0900

    MemberDTO 작성

--- src/main/java/com/example/pj2be/domain/MemberDTO.java
+++ src/main/java/com/example/pj2be/domain/MemberDTO.java
@@ -2,6 +2,17 @@ package com.example.pj2be.domain;
 
 import lombok.Data;
 
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
 @Data
 public class MemberDTO {
+    private BigInteger id;
+    private String member_id;
+    private String password;
+    private String nickname;
+    private String email;
+    private String phone_number;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
 }
