197d14e935c7a06bce981a767e4d54da915bdb3f
JungMo <jmjm0611@naver.com>
2023-11-23 14:44:09 +0900

    intitial commit

---
+++ src/main/java/com/example/pj2be/Pj2BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pj2be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj2BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pj2BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pj2be/Pj2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pj2be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pj2BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
