c83a415c2a0d83a135ca61caf20f1f45e6bd59a7
JungMo <jmjm0611@naver.com>
2023-11-23 17:02:34 +0900

    기본적이 패키지 구조 변경2

--- src/main/java/com/example/pj2be/controller/boardcontroller/BoardController.java
+++ src/main/java/com/example/pj2be/controller/boardcontroller/BoardController.java
@@ -1,4 +1,11 @@
 package com.example.pj2be.controller.boardcontroller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
 public class BoardController {
 }
