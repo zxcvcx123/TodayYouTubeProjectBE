68fbe95b010327dd7584b70debe234a04a676990
kwangsik Kim <penggusic@gmail.com>
2023-11-28 12:42:21 +0900

    대댓글 crud 작업 중(c,r까지 완료)(광식)

--- src/main/java/com/example/pj2be/mapper/commentmapper/ReplyCommentMapper.java
+++ src/main/java/com/example/pj2be/mapper/commentmapper/ReplyCommentMapper.java
@@ -3,6 +3,9 @@ package com.example.pj2be.mapper.commentmapper;
 import com.example.pj2be.domain.comment.ReplyCommentDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ReplyCommentMapper {
@@ -12,4 +15,17 @@ public interface ReplyCommentMapper {
             VALUES (#{comment_id}, #{reply_comment}, #{member_id})
             """)
     int insert(ReplyCommentDTO replycomment);
+
+    @Select("""
+            SELECT r.id,
+                   r.comment_id,
+                   r.member_id,
+                   r.reply_comment,
+                   r.created_at,
+                   m.nickname
+            FROM reply_comment r
+            JOIN member m ON r.member_id = m.member_id
+            WHERE comment_id = #{comment_id}
+            """)
+    List<ReplyCommentDTO> selectByComment_id(Integer comment_id);
 }
