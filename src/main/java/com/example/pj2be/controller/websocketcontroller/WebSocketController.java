ee34cc65be2f75017d618e465ac69da6e5f95de1
JungMo <jmjm0611@naver.com>
2023-11-30 17:38:26 +0900

    소켓1 (정정모)

--- src/main/java/com/example/pj2be/controller/websocketcontroller/WebSocketController.java
+++ src/main/java/com/example/pj2be/controller/websocketcontroller/WebSocketController.java
@@ -1,17 +1,11 @@
 package com.example.pj2be.controller.websocketcontroller;
 
-import com.example.pj2be.domain.soket.Greeting;
-import com.example.pj2be.domain.soket.HelloMessage;
-import com.example.pj2be.service.websocketservice.WebSocketService;
-import jakarta.websocket.Session;
-import lombok.RequiredArgsConstructor;
+import com.example.pj2be.domain.socket.Greeting;
+import com.example.pj2be.domain.socket.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
-
-import java.io.IOException;
 
 @Controller
 public class WebSocketController {
