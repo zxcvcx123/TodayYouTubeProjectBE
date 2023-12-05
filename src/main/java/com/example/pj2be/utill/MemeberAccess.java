d7c47461b6946e7058b91b3e8eabfa1833fb754a
Nighteast <jung946244@naver.com>
2023-12-05 14:18:19 +0900

    seongwon util 작성 = 본인 검증, 로그인 검증

--- src/main/java/com/example/pj2be/utill/MemeberAccess.java
+++ src/main/java/com/example/pj2be/utill/MemeberAccess.java
@@ -2,9 +2,9 @@ package com.example.pj2be.utill;
 
 public class MemeberAccess {
 
-    public static boolean IsLoginMember(String memberId) {
+    public static boolean IsLoginMember(String loginId) {
         // memberId 넣으면 로그인 여부 확인 가능
-        if (memberId != null) {
+        if (loginId != null) {
             return true;
         } else {
             return false;
@@ -13,7 +13,6 @@ public class MemeberAccess {
     }
 
     public static boolean MemberChecked(String loginId, String writerId) {
-
         // memberId가 있는경우 첫번째 파라미터에 넣고
         // 작성자를 두번째 파라미터에 넣어서 서로 같은지 검증
 
