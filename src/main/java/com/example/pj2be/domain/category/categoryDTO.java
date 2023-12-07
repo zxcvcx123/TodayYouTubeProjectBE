91a9731d01bde180675c9fdb506d1e06f570ec78
Nighteast <jung946244@naver.com>
2023-12-07 14:40:14 +0900

    seongwon categoryDTO.java 생성

---
+++ src/main/java/com/example/pj2be/domain/category/categoryDTO.java
@@ -0,0 +1,11 @@
+package com.example.pj2be.domain.category;
+
+import lombok.Data;
+
+@Data
+public class categoryDTO {
+    private Integer id;
+    private String code;
+    private String name;
+    private String name_eng;
+}
