494d687ce87135944611cb33b00c7299ac18f3dc
Nighteast <jung946244@naver.com>
2023-12-10 01:50:06 +0900

    domain.admin.BoardDataDTO 생성

---
+++ src/main/java/com/example/pj2be/domain/admin/BoardDataDTO.java
@@ -0,0 +1,11 @@
+package com.example.pj2be.domain.admin;
+
+import lombok.Data;
+
+@Data
+public class BoardDataDTO {
+
+    private String name_eng;
+    private Integer count_category_board;
+    private Integer count_category_view;
+}
