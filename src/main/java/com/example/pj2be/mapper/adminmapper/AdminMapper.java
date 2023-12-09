4cf4a3c4e8b10f8db6f720a4b800540d3de5e34b
Nighteast <jung946244@naver.com>
2023-12-10 01:50:38 +0900

    카테고리 별 게시글 수, 조회수 가져오기

--- src/main/java/com/example/pj2be/mapper/adminmapper/AdminMapper.java
+++ src/main/java/com/example/pj2be/mapper/adminmapper/AdminMapper.java
@@ -1,6 +1,6 @@
 package com.example.pj2be.mapper.adminmapper;
 
-import com.example.pj2be.domain.board.BoardDTO;
+import com.example.pj2be.domain.admin.BoardDataDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,8 +9,14 @@ import java.util.List;
 @Mapper
 public interface AdminMapper {
     @Select("""
-        SELECT *
-        FROM board
+        SELECT
+            c.name_eng,
+            COUNT(board_category_code) AS count_category_board,
+            COUNT(views) AS count_category_view
+        FROM board b
+            JOIN category c on b.board_category_code = c.code
+        GROUP BY board_category_code
+        ORDER BY board_category_code;
         """)
-    List<BoardDTO> getBoardData();
+    List<BoardDataDTO> getBoardData();
 }
