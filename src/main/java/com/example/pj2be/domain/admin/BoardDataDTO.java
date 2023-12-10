b09b279331c6cb73fd3996ddd67fa0975bb5ab7f
Nighteast <jung946244@naver.com>
2023-12-11 00:53:39 +0900

    카테고리 별 게시글 성비 추가

--- src/main/java/com/example/pj2be/domain/admin/BoardDataDTO.java
+++ src/main/java/com/example/pj2be/domain/admin/BoardDataDTO.java
@@ -5,7 +5,9 @@ import lombok.Data;
 @Data
 public class BoardDataDTO {
 
-    private String name_eng;
-    private Integer count_category_board;
-    private Integer count_category_view;
+    private String name_eng;                // 게시판 카테고리
+    private Integer count_category_board;   // 카테고리 별 게시글 작성 수
+    private Integer count_category_view;    // 카테고리 별 게시글 조회수 합
+    private Integer count_category_members_man;      // 카테고리 별 글작성 성비 (남)
+    private Integer count_category_members_woman;    // 카테고리 별 글작성 성비 (여)
 }
--- src/main/java/com/example/pj2be/mapper/adminmapper/AdminMapper.java
+++ src/main/java/com/example/pj2be/mapper/adminmapper/AdminMapper.java
@@ -12,9 +12,12 @@ public interface AdminMapper {
         SELECT
             c.name_eng,
             COUNT(board_category_code) AS count_category_board,
-            COUNT(views) AS count_category_view
+            COUNT(views) AS count_category_view,
+            SUM(CASE WHEN m.gender = 'm' THEN 1 ELSE 0 END) AS count_category_members_man,
+            SUM(CASE WHEN m.gender = 'w' THEN 1 ELSE 0 END) AS count_category_members_woman
         FROM board b
             JOIN category c on b.board_category_code = c.code
+            JOIN member m on b.board_member_id = m.member_id
         GROUP BY board_category_code
         ORDER BY board_category_code;
         """)
