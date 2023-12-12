dcd808fd482fff6430e7ff6210eeb6ad45ca8354
Nighteast <jung946244@naver.com>
2023-12-12 17:25:55 +0900

    방문자 조회수 - 현재시간 기준 30분 내에 중복 집계 허용하지 않음(서비스에서 검증)

--- src/main/java/com/example/pj2be/service/visitorservice/VisitorService.java
+++ src/main/java/com/example/pj2be/service/visitorservice/VisitorService.java
@@ -12,7 +12,16 @@ public class VisitorService {
 
     public void visitorCount(String clientIp, String member_id) {
 
-        visitorMapper.visitorInsert(clientIp, member_id);
+        System.out.println("member_id = " + member_id);
+
+        // 30분 전에 member_id로 찍힌 방문 기록이 있는지 조회
+        int countRecentVisit = visitorMapper.countRecentVisit(member_id);
+        System.out.println("countRecentVisit = " + countRecentVisit);
+
+        // 30분 전에 찍힌 기록이 없다면 insert 수행
+        if (countRecentVisit == 0) {
+            visitorMapper.visitorInsert(clientIp, member_id);
+        }
 
     }
 
