db6c862d7261da93792a86c30769f823b9f4034f
Nighteast <jung946244@naver.com>
2023-12-12 15:57:37 +0900

    seongwon 메인페이지 접속시 IP주소와 member_id 받아오기

--- src/main/java/com/example/pj2be/controller/visitorcontroller/VisitorController.java
+++ src/main/java/com/example/pj2be/controller/visitorcontroller/VisitorController.java
@@ -1,8 +1,10 @@
 package com.example.pj2be.controller.visitorcontroller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -13,8 +15,14 @@ import java.util.Map;
 public class VisitorController {
 
     @GetMapping("visitor")
-    public Integer boardDataList() {
+    public Integer boardDataList(HttpServletRequest request,
+                                 @RequestParam(required = false) String member_id) {
+        // IP 주소 얻기
+        String clientIp = request.getRemoteAddr();
 
+        // 정보 출력 (디버깅)
+        System.out.println("Client IP Address: {}" + clientIp);
+        System.out.println("member id: " + member_id);
         System.out.println("방문자 컨트롤러");
 
         return null;
