cdff0cf1031a1dd04dc5e82264a150e25abc5f37
Nighteast <jung946244@naver.com>
2023-12-13 10:43:25 +0900

    주석달기

--- src/main/java/com/example/pj2be/mapper/visitormapper/VisitorMapper.java
+++ src/main/java/com/example/pj2be/mapper/visitormapper/VisitorMapper.java
@@ -7,18 +7,20 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface VisitorMapper {
 
+    /**
+     * 방문자 기록하기. 사용자 ID, 클라이언트 IP
+     */
     @Insert("""
         INSERT INTO visitor_statistics (member_id, ip_address) 
             VALUES (#{member_id}, #{clientIp})
         """)
     void visitorInsert(String clientIp, String member_id);
 
-    @Select("""
-        SELECT COUNT(*)
-        FROM visitor_statistics;
-        """)
-    Integer visitorCount();
-
+    /**
+    * 방문자 기록 시 30분 전 이력 있다면 제외 <br>
+     * 30분 전 이력 카운트 해서 0이면 통과
+     * @return Integer
+    * */
     @Select("""
         SELECT COUNT(*)
         FROM visitor_statistics
@@ -26,4 +28,16 @@ public interface VisitorMapper {
             AND inserted_at > NOW() - INTERVAL 30 MINUTE ;
         """)
     Integer countRecentVisit(String memberId);
+
+    /**
+     * 전체 방문자 수 집계
+     * @return Integer
+     */
+    @Select("""
+        SELECT COUNT(*)
+        FROM visitor_statistics;
+        """)
+    Integer visitorCountAll();
+
+
 }
