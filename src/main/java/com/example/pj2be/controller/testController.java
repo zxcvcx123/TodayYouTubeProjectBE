69825d82969431e6f954a1ef514e346ee68c98d9
JungMo <jmjm0611@naver.com>
2023-12-14 13:38:27 +0900

    통합검색 완료 유효성 시작 - 1- (정정모)

---
+++ src/main/java/com/example/pj2be/controller/testController.java
@@ -0,0 +1,16 @@
+package com.example.pj2be.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+
+    private
+
+    public void test(){
+
+    }
+
+}
