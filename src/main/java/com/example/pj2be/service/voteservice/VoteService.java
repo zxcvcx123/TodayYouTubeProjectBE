04abb1292c18097d54ad341460bbb57cd414a658
JungMo <jmjm0611@naver.com>
2023-12-14 14:42:59 +0900

    토큰에러 (정정모)

--- src/main/java/com/example/pj2be/service/voteservice/VoteService.java
+++ src/main/java/com/example/pj2be/service/voteservice/VoteService.java
@@ -30,9 +30,31 @@ public class VoteService {
     }
 
 
-  // 투표 게시글 보기
-    public  VoteDTO view(Integer id){
+    // 투표 게시글 보기
+    public VoteDTO view(Integer id) {
         return voteMapper.view(id);
     }
 
+    // 게시글 리스트 보기
+    public Map<String, Object> list(PageDTO pageDTO, Integer p, String k) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        // 페이지
+        pageDTO.setPage(p);
+
+        // 검색
+        if (k == null || k.isBlank() || k.isEmpty()) {
+            pageDTO.setTotalList(voteMapper.getTotal("%%"));
+            map.put("vote", voteMapper.list(pageDTO, "%%"));
+        } else {
+            pageDTO.setTotalList(voteMapper.getTotal("%" + k + "%"));
+            map.put("vote", voteMapper.list(pageDTO, "%" + k + "%"));
+        }
+
+        map.put("page", pageDTO);
+        System.out.println("페이지: " + pageDTO);
+        System.out.println("내용: " + map.get("vote"));
+        return map;
+    }
 }
